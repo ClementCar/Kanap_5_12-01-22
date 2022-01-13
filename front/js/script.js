013162e50a575457a310fa020f2005b3b61fbf20
Clement Carcenac <clementcarcenac@gmail.com>
2022-01-13 20:05:29 +0100

    Récupération données api

---
+++ front/js/script.js
@@ -0,0 +1,62 @@
+// Récupere les produits de l'API
+const retrieveProductsData = () => {
+    fetch('http://localhost:3000/api/products')
+    .then(res = res.json())
+    .then(data = data.products)
+    .catch(err = console.log("erreur", err))
+}
+
+
+// Récupere la section où s'affichera les liens des canapés
+const $items = document.querySelector('.items')  
+
+// Création des images produits
+const createProductImg = product => {
+    const $productImg = document.createElement('img')
+
+    $productImg.setAttribute('src', `../../back/${product.imageUrl}`)
+    $productImg.setAttribute('alt', `${product.altTxt}`)
+    
+    return $productImg
+}
+
+
+// Création des Titre produits
+const createProductTitle = product => {
+    const $productTitle = document.createElement('h3')
+
+    $productTitle.textContent(`${product.name}`)
+    $productTitle.classList.add('ProductName')
+
+    return $productTitle
+}
+
+
+// Création des descriptions produits
+const createProductDescription = product => {
+    const $productDescription = document.createElement('p')
+
+    $productDescription.textContent(`${product.description}`)
+    $productDescription.classList.add('productDescription')
+
+    return $productDescription
+}
+
+// Création des liens produits complets
+const createProductCard = product => {
+    const $productLinks = document.createElement('a')
+    $productLinks.setAttribute('href', `./product.html?id=${product._id}`)
+
+    const $productArticle = document.createElement('article')
+
+    const $productImg = createProductImg(product)
+    const $productTitle = createProductTitle(product)
+    const $productDescription = createProductDescription(product)
+
+    $productLinks.appendChild($productArticle)
+    $productArticle.appendChild($productImg)
+    $productArticle.appendChild($productTitle)
+    $productArticle.appendChild($productDescription)
+
+    return $productLinks
+}
