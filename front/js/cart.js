b964c56348a7f5c31f1fa75325db858b4e9e09bb
Clement Carcenac <clementcarcenac@gmail.com>
2022-01-20 16:14:07 +0100

    Ajout dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,9 @@
-// récupere la secytion du panier 
+// récupere la section du panier 
 const $cart__items = document.getElementById('cart__items')
 
-//
+// Afficher le produit via l'API
+const showApiProduct = product => {
+    const $productArticle = document.createElement('article')
+    $productArticle.classList.add('cart__item')
+    $productArticle.setAttribute('data-id', `${product._id}`)
+}
--- front/js/product.js
+++ front/js/product.js
@@ -86,19 +86,47 @@ retrieveProductsData()
 // ajout d'élément dans le panier 
 const addCart = document.getElementById('addToCart')
 addCart.addEventListener('click', event => {
-    var count = localStorage.length
-    count++; 
+
+    // recherche si le panier est vide 
+    if (localStorage.length == 0) {
+        var count = 0
+    } else {
+        var count = localStorage.length 
+    }
+    console.log(count)
     var cart = {
         id : findProduct(),
         quantity : document.getElementById('quantity').value,
         colors : document.getElementById('colors').value 
     }
-    for ( element = 0 ; element < localStorage.length ; element++) {
-        if ((cart.id === element.id)&&(element.colors === cart.colors)){
-            element.quantity += cart.quantity
-        } else {
+    var same = false
+    // Si le panier n'est pas vide on recherche si le produit qu'on veut ajouter est deja dedans
+    if ( count > 0) {
+        for ( element = 0 ; element < localStorage.length ; element++) {
+            var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
+            console.log(storage)
+
+            // Si le produit est déja dans le panier, on le remplace avec la nouvelle quantité
+            if ((cart.id === storage.id)&&(storage.colors === cart.colors)) {
+                same = true
+                console.log(same)
+                console.log(cart.quantity)
+                console.log(storage.quantity)
+                var newQuantity = parseInt(cart.quantity) + parseInt(storage.quantity)
+                console.log(newQuantity)
+                cart.quantity = newQuantity
+                console.log(cart.quantity)
+                localStorage.removeItem(`cartStorage${element}`)
+                localStorage.setItem(`cartStorage${element}`, JSON.stringify(cart))
+            }
+        }
+        // Si le produit n'est pas dans le panier alors on l'ajoute
+        if (same == false) {
+            console.log(same)
             localStorage.setItem(`cartStorage${count}`, JSON.stringify(cart))
         }
+    } else {
+        localStorage.setItem(`cartStorage${count}`, JSON.stringify(cart))
     }
-    console.log(localStorage)
+    // console.log(localStorage)
 })
