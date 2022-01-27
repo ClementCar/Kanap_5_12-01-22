516403e83da92e048672cc68c928e7cccbb2ff9e
Clement Carcenac <clementcarcenac@gmail.com>
2022-01-27 16:33:18 +0100

    Finalisation

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,20 @@
 var totalQuantity = 0
 var totalPrice = 0
 
-// retrouve pr
-const retrievedddd = (id) => {
-    fetch(`http://127.0.0.1:3000/api/products/${id}`)
+// Déclaration de variable à envoyer à l'API
+var products = new Array()
+var contact = new Object()
+var sendOrder = {
+    contact,
+    products
+}
+
+// Récupere les produits de l'API pour récupérer le prix
+const retrieveProductsData = () => {
+    fetch('http://localhost:3000/api/products')
     .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        return(data);
+    .then(function(data) {
+        main(data);
     })
     .catch(err => console.log("erreur", err))
 }
@@ -17,16 +24,16 @@ const retrievedddd = (id) => {
 const $cart__items = document.getElementById('cart__items')
 
 // Afficher le produit via l'API et les infos de local Storage
-const showApiProduct = (product, pColor, pQuantity) => {
+const showApiProduct = (product, price) => {
     const $productArticle = document.createElement('article')
     $productArticle.classList.add('cart__item')
-    $productArticle.setAttribute('data-id', `${product._id}`)
-    $productArticle.setAttribute('data-color', pColor)
+    $productArticle.setAttribute('data-id', `${product.id}`)
+    $productArticle.setAttribute('data-color', `${product.colors}`)
 
     const $itemImage = document.createElement('div')
     $itemImage.classList.add('cart__item__img')
     const $image = document.createElement('img')
-    $image.setAttribute('src', `${product.imageUrl}`)
+    $image.setAttribute('src', `${product.image}`)
     $image.setAttribute('alt', `${product.altTxt}`)
     $itemImage.appendChild($image)
 
@@ -38,9 +45,9 @@ const showApiProduct = (product, pColor, pQuantity) => {
     const $descriptionName = document.createElement('h2')
     $descriptionName.textContent = `${product.name}`
     const $descriptionColor = document.createElement('p')
-    $descriptionColor.textContent = pColor
+    $descriptionColor.textContent = `${product.colors}`
     const $descriptionPrice = document.createElement('p')
-    $descriptionPrice.textContent = `${product.price}`
+    $descriptionPrice.textContent = `${price}`
     $contentDescription.appendChild($descriptionName)
     $contentDescription.appendChild($descriptionColor)
     $contentDescription.appendChild($descriptionPrice)
@@ -57,7 +64,7 @@ const showApiProduct = (product, pColor, pQuantity) => {
     $quantityInput.setAttribute('name', 'itemQuantity')
     $quantityInput.setAttribute('min', '1')
     $quantityInput.setAttribute('max', '100')
-    $quantityInput.setAttribute('value', `${pQuantity}`)
+    $quantityInput.setAttribute('value', `${product.quantity}`)
     $settingQuantity.appendChild($quantityP)
     $settingQuantity.appendChild($quantityInput)
     const $settingDelete = document.createElement('div')
@@ -79,40 +86,45 @@ const showApiProduct = (product, pColor, pQuantity) => {
     $cart__items.appendChild($productArticle)
 }
 
-// Recherche le produit dans l'API et appel l'affichage du panier avec 
-// l'id la couleur et la quantité contenu dans local Storage
-const retrieveProduct = async () => {
+// Appel l'affichage pour chaque produit du panier 
+const main = async (allProducts) => {
     var articleCart = localStorage.length
+    console.table(allProducts)
     if ( articleCart > 0) {
         for ( element = 0 ; element < localStorage.length ; element++) {
             var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
             console.log(storage)
-            const product = retrievedddd(storage.id)
-            console.table(product)
-            // main(product, storage.colors, storage.quantity)
+            products.push(storage.id)
+            for ( let product = 0 ; product < allProducts.length ; product++) {
+                if (allProducts[product]._id == storage.id) {
+                    console.log(storage)
+                    await showApiProduct(storage, allProducts[product].price)
+                    totalPrice += allProducts[product].price * storage.quantity
+                    totalQuantity += parseInt(storage.quantity)
+                    document.getElementById('totalQuantity').textContent = totalQuantity
+                    document.getElementById('totalPrice').textContent = totalPrice
+                }
+            }
         }
+        console.log(products)
+        listen()
+        listenForm()
+        productOrder()
     } else {
-        // message de panier vide 
+        // message de panier vide
+        $emptyCart = document.createElement('h2')
+        $emptyCart.textContent = "Le panier est vide"
+        $cart__items.appendChild($emptyCart)
+        listenForm()
+        productOrder()
     }
 }
 
-// Appel l'affichage pour chaque produit du panier 
-const main = (data, color, quantity) => {
-    console.table(data)
-    showApiProduct(data, color, quantity)
 
-    // A chaque passage, le prix * la quantité et la quantité des produit s'accumulent et s'affichent 
-    totalPrice += data.price * quantity
-    totalQuantity += parseInt(quantity)
-    document.getElementById('totalQuantity').textContent = totalQuantity
-    document.getElementById('totalPrice').textContent = totalPrice
-    console.log(document.querySelectorAll('.deleteItem'))
-    listen()
-}
-
-retrieveProduct()
-
+// Fonction de suppression et de changement de quantité
 function listen() {
+
+    // Suppression d'élement du panier
     var deleteItem = document.querySelectorAll('.deleteItem')
     console.log(deleteItem)
     console.log('on est la')
@@ -122,53 +134,243 @@ function listen() {
         const $deleteColor = $deleteArticle.dataset.color
         console.log($deleteId)
         console.log($deleteColor)
-        // for ( element = 0 ; element < localStorage.length ; element++) {
-        //     var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
-            
-        //     // Si le produit est déja dans le panier, on le remplace avec la nouvelle quantité
-        //     if (( $deleteId === storage.id)&&(storage.colors === $deleteColor)) {
-        //         localStorage.removeItem(`cartStorage${element}`)
-        //     }
-        // }
-        
+        const lastLength = localStorage.length
+        for ( element = 0 ; element < localStorage.length ; element++) {
+            var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
+            console.log(storage.id)
+            console.log(storage.colors)
+
+            // Le produit sélectionné est supprimé de local Storage
+            if (( $deleteId === storage.id)&&(storage.colors === $deleteColor)) {
+                localStorage.removeItem(`cartStorage${element}`)
+
+                // On remet les numéros de local Storage dans l'ordre pour qu'il n'y ai pas de vide
+                for ( newNumb = element + 1 ; newNumb < lastLength ; newNumb++) {
+                    var newStorage = JSON.parse(localStorage.getItem(`cartStorage${newNumb}`))
+                    localStorage.removeItem(`cartStorage${newNumb}`)
+                    localStorage.setItem(`cartStorage${newNumb - 1}`, JSON.stringify(newStorage))
+                }
+            }
+        }
+        window.location.reload()
+        alert('Produit supprimé du panier')
+    }))
+
+    // Changement de quantité
+    var changeQuantity = document.querySelectorAll('.itemQuantity')
+    changeQuantity.forEach(changeQuantity => changeQuantity.addEventListener('change', event => {
+        const $changeArticle = changeQuantity.closest('article')
+        const $changeId = $changeArticle.dataset.id
+        const $changeColor = $changeArticle.dataset.color
+        console.log($changeColor)
+        console.log($changeId)
+        console.log(changeQuantity.value)
+        const lastLength = localStorage.length
+        for ( element = 0 ; element < localStorage.length ; element++) {
+            var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
+    
+            // On remplace le produit avec la nouvelle quantité
+            if (($changeId === storage.id)&&(storage.colors === $changeColor)) {
+                storage.quantity = changeQuantity.value
+                if (storage.quantity > 0) {
+                    localStorage.removeItem(`cartStorage${element}`)
+                    localStorage.setItem(`cartStorage${element}`, JSON.stringify(storage))
+                } else {
+                    localStorage.removeItem(`cartStorage${element}`)
+                    for ( newNumb = element + 1 ; newNumb < lastLength ; newNumb++) {
+                        var newStorage = JSON.parse(localStorage.getItem(`cartStorage${newNumb}`))
+                        localStorage.removeItem(`cartStorage${newNumb}`)
+                        localStorage.setItem(`cartStorage${newNumb - 1}`, JSON.stringify(newStorage))
+                    }
+                }
+            }
+        }
+        window.location.reload()
+        alert('Changement de quantité effectué')
+
     }))
 }
-
-// changeQuantity.addEventListener('change', event => {
-//     const changeArticle = changeQuantity.closest('article')
-//     const $changeId = $deleteArticle.dataset.id
-//     const $changeColor = $deleteArticle.dataset.color
-//     for ( element = 0 ; element < localStorage.length ; element++) {
-//         var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
-
-//         // Si le produit est déja dans le panier, on le remplace avec la nouvelle quantité
-//         if (($changeId === storage.id)&&(storage.colors === $changeColor)) {
-//             var newQuantity = parseInt(cart.quantity) + parseInt(storage.quantity)
-//             storage.quantity = newQuantity
-//             localStorage.removeItem(`cartStorage${element}`)
-//             localStorage.setItem(`cartStorage${element}`, JSON.stringify(storage))
-//         }
-//     }
-// })
 
 
 
 
 
 // Récupération des données du formulaires
-const firstName = document.getElementById('firstName')
+const inputFirstName = document.getElementById('firstName')
 const firstNameError = document.getElementById('firstNameErrorMsg')
-const lastName = document.getElementById('lastName')
+const inputLastName = document.getElementById('lastName')
 const lastNameError = document.getElementById('lastNameErrorMsg')
-const address = document.getElementById('address')
+const inputAddress = document.getElementById('address')
 const addressError = document.getElementById('addressErrorMsg')
-const city = document.getElementById('city')
+const inputCity = document.getElementById('city')
 const cityError = document.getElementById('cityErrorMsg')
-const email = document.getElementById('email')
+const inputEmail = document.getElementById('email')
 const emailError = document.getElementById('emailErrorMsg')
+const order = document.getElementById('order')
 
 // Règles regex
-const validFlc = RegExp()
-const validAddress = RegExp()
-const validEmail = RegExp()
+const validName = RegExp(/^[A-zÀ-ú\-]+$/) // ne doit être composé que de lettres min ou maj et tiré
+const validCity = RegExp(/^[A-zÀ-ú\- ]+$/) // lettres min MAJ, espaces, une ou plusieurs fois
+const validAddress = RegExp(/^[0-9A-zÀ-ú\- ]+$/) // lettres min MAJ, chiffres, espaces, une ou plusieurs fois
+const validEmail = RegExp(/^[0-9a-z\-_.]+@[0-9a-z\-_.]+.[a-z]{2,3}$/)
 
+// Validation du prénom
+const validationFirstName = (name) => {
+    var valid = false
+    if (name.match(validName)) {
+        valid = true
+        firstNameError.textContent = ''
+    } else {
+        firstNameError.textContent = "Votre prénom n'est pas valide"
+    }
+    return valid
+}
+
+// Validation du nom
+const validationLastName = (name) => {
+    var valid = false
+    if (name.match(validName)) {
+        valid = true
+        lastNameError.textContent = ''
+    } else {
+        lastNameError.textContent = "Votre nom n'est pas valide"
+    }
+    return valid
+}
+
+// Validation de la ville
+const validationCity = (name) => {
+    var valid = false
+    if (name.match(validCity)) {
+        valid = true
+        cityError.textContent = ''
+    } else {
+        cityError.textContent = "Votre ville n'est pas valide"
+    }
+    return valid
+}
+
+// Validation de l'adresse
+const validationAddress = (name) => {
+    var valid = false
+    if (name.match(validAddress)) {
+        valid = true
+        addressError.textContent = ''
+    } else {
+        addressError.textContent = "Votre adresse n'est pas valide"
+    }
+    return valid
+}
+
+// Validation de l'email
+const validationEmail = (name) => {
+    var valid = false
+    if (name.match(validEmail)) {
+        valid = true
+        emailError.textContent = ''
+    } else {
+        emailError.textContent = "Votre email n'est pas valide"
+    }
+    return valid
+}
+
+
+// Déclaration des variables de validations de formulaires
+var fName = false
+var lName = false
+var vCity = false
+var vAddress = false
+var vEmail = false
+
+
+// Validation du formulaire, retourne true si il est bien rempli
+const listenForm = () => {
+    inputFirstName.addEventListener('change', event => {
+        fName = validationFirstName(inputFirstName.value)
+        listenForm()
+    })
+    inputLastName.addEventListener('change', event => {
+        lName = validationLastName(inputLastName.value)
+        listenForm()
+    })
+    inputCity.addEventListener('change', event => {
+        vCity = validationCity(inputCity.value)
+        listenForm()
+    })
+    inputAddress.addEventListener('change', event => {
+        vAddress = validationAddress(inputAddress.value)
+        listenForm()
+    })
+    inputEmail.addEventListener('change', event => {
+        vEmail = validationEmail(inputEmail.value)
+        listenForm()
+    })
+    if ((fName == true)&&(lName == true)&&(vCity == true)&&(vAddress == true)&&(vEmail == true)) {
+        contact.firstName = inputFirstName.value
+        contact.lastName = inputLastName.value
+        contact.address = inputAddress.value
+        contact.city = inputCity.value
+        contact.email = inputEmail.value
+        return true
+    } else {
+        return false
+    }
+}
+
+// Envoie du formulaire et du panier à l'API
+const productOrder = () => {
+    order.addEventListener('click', event => {
+        if (localStorage.length > 0) {
+            var validOrder = listenForm()
+            console.log(validOrder)
+            console.log(sendOrder)
+            console.log(JSON.stringify(sendOrder))
+            if (validOrder == true) {
+                alert('Formulaire valide')
+                fetch("http://localhost:3000/api/products/order", {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(sendOrder)
+                })
+                .then(res => res.json())
+                .then(function(data) {
+                    console.log(data)
+                    document.location.href = `http://127.0.0.1:5500/front/html/confirmation.html?orderId=${data.orderId}`
+                    localStorage.clear()
+                })
+                .catch(err => console.log("erreur", err))
+            } else {
+                alert('Formulaire non valide')
+            }
+        } else {
+            alert('Votre panier est vide')
+        }
+    })
+}
+
+// Afficher le numéro de commande dans la page confirmation
+const showOrderId = () => {
+    const showId = document.getElementById("orderId")
+    var string = window.location.href
+    var url = new URL(string)
+    const $idOrder = url.searchParams.get('orderId')
+    showId.textContent = $idOrder
+}
+
+// recherche la page courante et appel les programmes à effectuer
+const pages = () => {
+    var string = window.location.href
+    var cartPage = /cart/
+    var find = string.search(cartPage)
+    if (find>-1) {
+        retrieveProductsData()
+    } else {
+        showOrderId()
+    }
+    
+}
+
+pages()
--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,6 @@
 // Récupere l'id produit de la page et recherche dans l'api les données du produit
 const retrieveProductsData = () => {
-    var string = window.location.href
-    var url = new URL(string)
-    const $idProducts = url.searchParams.get('id')
+    const $idProducts = findProduct()
     fetch(`http://127.0.0.1:3000/api/products/${$idProducts}`)
     .then(res => res.json())
     .then(function(data) {
@@ -26,6 +24,7 @@ const $itemTitle = document.getElementById('title')
 const $itemPrice = document.getElementById('price')
 const $itemDescription = document.getElementById('description')
 const $itemColors = document.getElementById('colors')
+const $itemQuantity = document.getElementById('quantity')
 
 // ajout image produit
 const addingImage = product => {
@@ -86,52 +85,49 @@ retrieveProductsData()
 const listen = (data) => {
     const addCart = document.getElementById('addToCart')
     addCart.addEventListener('click', event => {
-
-        // recherche si le panier est vide 
-        if (localStorage.length == 0) {
-            var count = 0
-        } else {
+        if (($itemColors.value != "")&&($itemQuantity.value > 0)&&($itemQuantity.value < 101)) {
             var count = localStorage.length 
-        }
-        console.log(count)
-        var cart = {
-            id : findProduct(),
-            quantity : document.getElementById('quantity').value,
-            colors : document.getElementById('colors').value,
-            name : data.name,
-            image : data.imageUrl,
-            description : data.description,
-            altTxt : data.altTxt
-        }
-        var same = false
-        // Si le panier n'est pas vide on recherche si le produit qu'on veut ajouter est deja dedans
-        if ( count > 0) {
-            for ( element = 0 ; element < localStorage.length ; element++) {
-                var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
-                console.log(storage)
-
-                // Si le produit est déja dans le panier, on le remplace avec la nouvelle quantité
-                if ((cart.id === storage.id)&&(storage.colors === cart.colors)) {
-                    same = true
-                    console.log(same)
-                    console.log(cart.quantity)
-                    console.log(storage.quantity)
-                    var newQuantity = parseInt(cart.quantity) + parseInt(storage.quantity)
-                    console.log(newQuantity)
-                    cart.quantity = newQuantity
-                    console.log(cart.quantity)
-                    localStorage.removeItem(`cartStorage${element}`)
-                    localStorage.setItem(`cartStorage${element}`, JSON.stringify(cart))
-                }
+            console.log(count)
+            var cart = {
+                id : findProduct(),
+                quantity : document.getElementById('quantity').value,
+                colors : document.getElementById('colors').value,
+                name : data.name,
+                image : data.imageUrl,
+                description : data.description,
+                altTxt : data.altTxt
             }
-            // Si le produit n'est pas dans le panier alors on l'ajoute
-            if (same == false) {
-                console.log(same)
+            var same = false
+            // Si le panier n'est pas vide on recherche si le produit qu'on veut ajouter est deja dedans
+            if ( count > 0) {
+                for ( element = 0 ; element < localStorage.length ; element++) {
+                    var storage = JSON.parse(localStorage.getItem(`cartStorage${element}`))
+                    console.log(storage)
+
+                    // Si le produit est déja dans le panier, on le remplace avec la nouvelle quantité
+                    if ((cart.id === storage.id)&&(storage.colors === cart.colors)) {
+                        same = true
+                        console.log(same)
+                        console.log(cart.quantity)
+                        console.log(storage.quantity)
+                        var newQuantity = parseInt(cart.quantity) + parseInt(storage.quantity)
+                        console.log(newQuantity)
+                        cart.quantity = newQuantity
+                        console.log(cart.quantity)
+                        localStorage.removeItem(`cartStorage${element}`)
+                        localStorage.setItem(`cartStorage${element}`, JSON.stringify(cart))
+                    }
+                }
+                // Si le produit n'est pas dans le panier alors on l'ajoute
+                if (same == false) {
+                    console.log(same)
+                    localStorage.setItem(`cartStorage${count}`, JSON.stringify(cart))
+                }
+            } else {
                 localStorage.setItem(`cartStorage${count}`, JSON.stringify(cart))
             }
         } else {
-            localStorage.setItem(`cartStorage${count}`, JSON.stringify(cart))
+            alert('Veuillez préciser le couleur et la quantité')
         }
-        // console.log(localStorage)
     })
 }
